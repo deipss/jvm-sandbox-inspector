ece0d47428349cdcfe7af79735eaa4aa1111bb5e
deipss <deipss666@gmail.com>
2023-12-25 22:58:50 +0800

    ttl

---
+++ jvm-sandbox-inspector-agent/jvm-sandbox-inspector-agent-core/src/main/java/me/deipss/jvm/sandbox/inspector/agent/core/plugin/concurrent/TtlConcurrentListener.java
@@ -0,0 +1,31 @@
+package me.deipss.jvm.sandbox.inspector.agent.core.plugin.concurrent;
+
+import com.alibaba.jvm.sandbox.api.listener.ext.Advice;
+import com.alibaba.jvm.sandbox.api.listener.ext.AdviceListener;
+import com.alibaba.ttl.TtlCallable;
+import com.alibaba.ttl.TtlRunnable;
+
+import java.util.concurrent.Callable;
+
+
+public class TtlConcurrentListener extends AdviceListener{
+    @Override
+    protected void before(Advice advice) throws Throwable {
+        // 包装成ttl
+        final Object[] parameterArray = advice.getParameterArray();
+        final Class<?>[] parameterTypeArray = advice.getBehavior().getParameterTypes();
+        if (parameterArray == null || parameterArray.length < 1) {
+            return;
+        }
+        if (parameterArray[0] instanceof com.alibaba.ttl.TtlEnhanced) {
+            return;
+        }
+        Class<?> parameter0Type = parameterTypeArray[0];
+        if (parameter0Type.isAssignableFrom(Runnable.class)) {
+            parameterArray[0] = TtlRunnable.get((Runnable) parameterArray[0]);
+        }
+        if (parameter0Type.isAssignableFrom(Callable.class)) {
+            parameterArray[0] = TtlCallable.get((Callable) parameterArray[0]);
+        }
+    }
+}
---
+++ jvm-sandbox-inspector-agent/jvm-sandbox-inspector-agent-core/src/main/java/me/deipss/jvm/sandbox/inspector/agent/core/plugin/concurrent/TtlConcurrentPlugin.java
@@ -0,0 +1,21 @@
+package me.deipss.jvm.sandbox.inspector.agent.core.plugin.concurrent;
+
+import com.alibaba.jvm.sandbox.api.listener.ext.EventWatchBuilder;
+import com.alibaba.jvm.sandbox.api.resource.ModuleEventWatcher;
+
+
+public class TtlConcurrentPlugin {
+    public synchronized void watch(ModuleEventWatcher watcher) {
+        new EventWatchBuilder(watcher)
+                .onClass("java.util.concurrent.ThreadPoolExecutor").includeBootstrap()
+                .onBehavior("execute")
+                .onBehavior("submit")
+                .onClass("java.util.concurrent.ScheduledThreadPoolExecutor").includeBootstrap()
+                .onBehavior("execute")
+                .onBehavior("submit")
+                .onBehavior("schedule")
+                .onBehavior("scheduleAtFixedRate")
+                .onBehavior("scheduleWithFixedDelay")
+                .onWatch(new TtlConcurrentListener());
+    }
+}
